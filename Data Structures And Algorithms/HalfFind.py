813a89815378182cbd149918e6ed0b8c45172fee
MingRui.Hu <497025126@qq.com>
2018-01-29 22:33:42 +0800

    fix some bug

--- Data Structures And Algorithms/HalfFind.py
+++ Data Structures And Algorithms/HalfFind.py
@@ -1,30 +1,34 @@
 # 残品折半  从小到大
 def halfUp(li,data):
     mid = len(li) // 2
-    if li[mid] > data :
-        for i in range(mid-1,-1,-1):
+    if li[mid] > data : # 中间数字大于要查找数字 向左边查找
+        for i in range(mid-1,0,-1):
             if li[i] == data:
                 return i
         return 'errorLeft'
-    else:
-        for i in range(mid,len(li)):
+    elif li[mid] == data:
+        return mid
+    else:#否则向右边查找
+        for i in range(mid+1,len(li)):
             if li[i] == data:
                 return i
         return 'errorRight'
 
-# 残品折半  从大到小
+# 残品折半  从大到小 10 9 8 7 6 5 4 3 2 1
 def halfDown(li, data):
     mid = len(li) // 2
     if li[mid] > data:
-        for i in range(mid - 1, -1, -1):
-            if li[i] == data:
-                return i
-        return 'errorLeft'
-    else:
-        for i in range(mid, len(li)):
+        for i in range(mid+1,len(li)):
             if li[i] == data:
                 return i
         return 'errorRight'
+    elif li[mid] == data:
+        return mid
+    else:
+        for i in range(mid-1,0,-1):
+            if li[i] == data:
+                return i
+        return 'errorLeft'
 
 # 标准折半查找 升序序列查找
 def FullhalfUp(li,data):
@@ -57,6 +61,6 @@ def FullhalfDown(li,data):
 
 if __name__ =='__main__':
     li = [1, 2, 3, 4, 5, 7, 9]
-    li = [4,3,2,1]
-    findFunc = FullhalfDown
+    # li = [10,9,8,7,6,5,4,3,2,1]
+    findFunc = halfUp
     print(findFunc(li,4))
